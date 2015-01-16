3b175dfffb526abca7dbfa27bc3cbea189bbcc13
moinadil90 <moinadil90@gmail.com>
2015-01-17 02:12:34 +0530

    Check

---
+++ SlidingMenu/src/com/traxbuddy/trax/HelpFragment.java
@@ -0,0 +1,26 @@
+package com.traxbuddy.trax;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelpFragment extends Fragment {
+	
+	public HelpFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+		
+        View rootView = inflater.inflate(R.layout.fragment_help, container, false);
+         
+        return rootView;
+     
+
+    }
+	
+}
